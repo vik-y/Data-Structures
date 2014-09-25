b23cbaf19aa12a598ff5e8b702814d29f3fc5688
bobo <vikasyadavgood@gmail.com>
2014-09-25 19:25:22 +0530

    b-tree started

---
+++ B-Tree/btree.c
@@ -0,0 +1,28 @@
+/*
+ * Coding the B-Tree which is used in almost all the popular databases
+ * @Vikas Yadav github.com/vik-y
+ * NOTE: Dynamic memory allocation is the easiest way to store the strings in C
+ * and that's how upper level language do it.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int search(){
+	return 1;
+}
+
+void insert(){
+	//Insert a new node into the b-tree
+}
+
+int delete(){
+	//return 1 if successfully deleted else return 0
+	return 1;
+}
+
+int main(int argc, char **argv) {
+	//B-Tree is famous for the speed it has, it does almost everything with Order log(n)
+	//insertion - log(n), deletion - log(n), search - log(n)
+	//A simple way to judge the efficiency is - suppose you have 1 million entries into your db
+}
